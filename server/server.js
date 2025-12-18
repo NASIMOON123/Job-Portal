47ba11acc994d85b7f7c09f56ec26edf4feb75b2
NASIMOON123 <mnasimoon15@gmail.com>
2025-12-18 10:51:29 +0530

    Add root route and deployment fixes

--- server/server.js
+++ server/server.js
@@ -48,6 +48,10 @@
   app.use('/api/admin', AdminRoutes);
   app.use('/api/applications', applicationRoutes);
   const PORT = process.env.PORT || 5000;
+  app.get("/", (req, res) => {
+    res.send("Job Portal Backend is running 🚀");
+  });
+  
   app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
   });
